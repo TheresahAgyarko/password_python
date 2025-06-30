4124957c64f5c01634438a955f6f7704b6de3b25
Gideon Amoah <giderraamoah777@gmail.com>
2025-06-30 16:56:46 +0000

    update 4

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,10 @@
 import string
 import random
-character=string.ascii_letters + string.digits + string.punctuation  #ascci is use to generate both lower  and uppercase letters
+ #ascci is use to generate both lower  and uppercase letters
 
 # using function
-def password_generator(length=12,By_punctuations=True,By_numbers=True): #Definding function
-    password=''.join(random.choice(character)for i in range(12))
-    return password
+def password_generator(length=12,character=string.ascii_letters + string.digits + string.punctuation): #Definding function
+    return''.join(random.choice(character)for i in range(12))
 
 option=input("Do you want to generate a password (Yes/No) : ")
 
