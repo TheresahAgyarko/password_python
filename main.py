92df8ad67cbd8f565a168c4077c9c405ca889569
TheresahAgyarko <agyarkothess@icloud.com>
2025-07-01 20:02:54 +0000

    update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import password_generator
-my_password=password_generator
-user_input=input("Please type continue to generate your password : ")
-final_password=my_password.password_generator(user_input)
-print(f' This is your password : {final_password}')
+
+while True:
+    my_password=password_generator
+    user_input=input("Please type continue to generate your password : ")
+    final_password=my_password.password_generator(user_input)
+    print(f' This is your password : {final_password}')
--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,16 @@
 import string
 import random
- #ascci is use to generate both lower  and uppercase letters
+ 
 
 # using function
-def password_generator(length=12,character=string.ascii_letters + string.digits + string.punctuation): #Definding function
+def password_generator(length=12,character=string.ascii_letters + string.digits + string.punctuation): #function
     return''.join(random.choice(character)for i in range(12))
 
 option=input("Do you want to generate a password (Yes/No) : ")
 
 # using control structure
 if option=="Yes":
-    # ivoke the function
+    
     password_generator()
 elif option=="No":
     print("Program end") 
